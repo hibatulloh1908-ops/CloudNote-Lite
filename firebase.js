cfae3746a3b220a8054399a425140b9f0c306fd4
hibatulloh1908-ops <hibatulloh.1908@gmail.com>
2026-07-11 00:45:31 +0700

    Done firebase.js

--- firebase.js
+++ firebase.js
@@ -1,28 +1,15 @@
 // ===============================
 // CloudNote Lite
-// Firebase Config
+// firebase.js
 // ===============================
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
 import {
-  getFirestore,
-  collection,
-  doc,
-  getDoc,
-  setDoc,
-  addDoc,
-  updateDoc,
-  deleteDoc,
-  onSnapshot,
-  query,
-  where,
-  orderBy,
-  serverTimestamp
+  getFirestore
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
-// ===============================
-// GANTI DENGAN FIREBASE CONFIG
-// ===============================
-
+// Firebase Config
 const firebaseConfig = {
   apiKey: "AIzaSyD_Ho3nI5d-n7lZf3jnD8E19JMpnMV8-eM",
   authDomain: "cloudnote-9bb7f.firebaseapp.com",
@@ -32,42 +19,11 @@ const firebaseConfig = {
   appId: "1:371579501428:web:aee883867bac141af14a21"
 };
 
-// ===============================
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// Firestore
 const db = getFirestore(app);
 
-const auth = getAuth(app);
-
-
-// Login Anonymous
-
-signInAnonymously(auth)
-.then(()=>{
-
-console.log("Anonymous Login Success");
-
-})
-.catch((err)=>{
-
-console.error(err);
-
-});
-
-export {
-  db,
-  auth,
-  collection,
-  doc,
-  getDoc,
-  setDoc,
-  addDoc,
-  updateDoc,
-  deleteDoc,
-  onSnapshot,
-  query,
-  where,
-  orderBy,
-  serverTimestamp
-};
+// Export
+export { db };
